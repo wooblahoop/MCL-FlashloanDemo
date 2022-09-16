ecab03f20a8e7d514856cc33d159f40ff1c4c072
wooblahoop <113727135+wooblahoop@users.noreply.github.com>
2022-09-16 22:55:50 +0200

    Update demo.sol

--- contracts/demo.sol
+++ contracts/demo.sol
@@ -1,59 +1,52 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.5.0;
 
-import "./base/FlashLoanReceiverBase.sol";
-import "./interfaces/ILendingPoolAddressesProvider.sol";
-import "./interfaces/ILendingPool.sol";
-import "./interfaces/IDefi.sol";
+interface IUniswapV2Pair {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
 
+    function name() external pure returns (string memory);
+    function symbol() external pure returns (string memory);
+    function decimals() external pure returns (uint8);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address owner) external view returns (uint);
+    function allowance(address owner, address spender) external view returns (uint);
 
-contract Demo is FlashLoanReceiverBase {
+    function approve(address spender, uint value) external returns (bool);
+    function transfer(address to, uint value) external returns (bool);
+    function transferFrom(address from, address to, uint value) external returns (bool);
 
-    address public constant BNB_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address public defi;
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+    function nonces(address owner) external view returns (uint);
 
-    constructor(ILendingPoolAddressesProvider _addressesProvider, address _defi)
-        public
-        FlashLoanReceiverBase(_addressesProvider)
-    {
-        defi = _defi;
-    }
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
 
-    function executeOperation(
-        address _reserve,
-        uint256 _amount,
-        uint256 _fee,
-        bytes calldata _params
-    ) external    {
-        require(
-            _amount <= getBalanceInternal(address(this), _reserve),
-            "Invalid balance, was the flashLoan successful?"
-        );
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
 
-        //
-        // Your logic goes here.
-        // !! Ensure that *this contract* has enough of `_reserve` funds to payback the `_fee` !!
-        //
+    function MINIMUM_LIQUIDITY() external pure returns (uint);
+    function factory() external view returns (address);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function price0CumulativeLast() external view returns (uint);
+    function price1CumulativeLast() external view returns (uint);
+    function kLast() external view returns (uint);
 
-        IDefi app = IDefi(defi);
-        // Todo: Deposit into defi smart contract
-        app.depositBNB.value(_amount)(_amount);
-        
-        // Todo: Withdraw from defi smart contract
-        app.withdraw(_amount);
+    function mint(address to) external returns (uint liquidity);
+    function burn(address to) external returns (uint amount0, uint amount1);
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
+    function skim(address to) external;
+    function sync() external;
 
-
-        uint256 totalDebt = _amount.add(_fee);
-        transferFundsBackToPoolInternal(_reserve, totalDebt);
-    }
-
-  
-    function flashloanBnb(uint256 _amount) public  {
-        bytes memory data = "";
-       
-        ILendingPool lendingPool = ILendingPool(
-            addressesProvider.getLendingPool()
-        );
-        lendingPool.flashLoan(address(this), BNB_ADDRESS, _amount, data);
-    }
-    
+    function initialize(address, address) external;
 }
